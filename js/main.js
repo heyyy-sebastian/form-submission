8c1a6d3b95a07d30bf6992838bae2245a715faa1
mermatriarchy <hailey.miller91@gmail.com>
2017-06-04 13:53:03 -0400

    add check fields fn

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     	$active.stop().slideUp("slow").removeClass('active');
    	}); //end form expansion/collapse
 
-	//show submission message
+	//show submission message, bind click event to OK button
    	var makeModal = function(message){
 		//append notification modal to DOM
 		$("body").append("<div class='submission-notice'><p class='blue-text'>"+ message + "</p><button id='ok'>OK</button></div>");
@@ -21,34 +21,55 @@ $(document).ready(function(){
 			e.preventDefault();
 			$('.submission-notice').remove();
 		});
-	};
+	}; //end make modal fn
+
+	//check that form fields are not null
+	var checkFields = function(message){
+		//remove whitespace & check for empty form fields
+		var emptyFields = $('form :input').filter(function() {
+            return $.trim(this.value) === "";
+        });
+		//if there are incomplete fields, trigger error notification
+        if (emptyFields.length) {
+            makeModal(message);
+            return
+        }
+	};//end form fields check
 
 	//attach handler to form submission
 	$('form').submit(function(e){
 		//prevent form from submitting normally
 		e.preventDefault();
 		//assign data from form fields to variables
-		form = $('form').serialize();
+		var form = $('form').serialize();
+		//set error message in case it's needed
+		var errorMessage = "There was an error completing your submission. Please check your information and try again."
+		//check that all fields are complete
+		checkFields(errorMessage);
 		//send data in post request	
 		$.post(
 			"https://httpbin.org/post", 
-			$('form').serialize(),			
+			form,			
 			function(data, status){
 				//hide feedback form after successful submission
 				$('.feedback-form').stop().slideUp("slow").removeClass('active');
 				var successMessage = "Thank you! Your submission has been recorded."
 				//trigger success notification
 				makeModal(successMessage);
-			})//end post request
+			},
+			"json")//end post request, add callback if the post request fails or if there's an error
+			.fail(function(){
+				//trigger errorMessage
+				makeModal(errorMessage);
+			})//end fail callback
 
 	}); //end form submission
 
 	//to-do
 	// ratings submission
-	// show thank you message
 	// error message
 
 	//padding around top row on load
-	//close/shrink form on submission
+	//media queries
 
 }); //end wrapper function
